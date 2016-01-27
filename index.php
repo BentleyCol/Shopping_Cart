75224b0d82f3e995effcbf80ffa4117f4bb6904b
Benjamin Cole <bentleycolchester@gmail.com>
2016-01-27 13:53:17 +1300

    Add database and show products on homepage

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Shopping Cart</title>
+	</head>
+	<body>
+
+	<h1>Products</h1>
+
+	<?php
+
+		//Connect to the DB
+		$dbc = new mysqli('localhost', 'root', '', 'shopping_cart');
+
+		// Get all the products from the DB
+		$sql = "SELECT id, name, description, price, stock FROM products";
+
+		//Run the Query
+		$result = $dbc->query($sql);
+
+		//Loop over the result
+		while($row = $result->fetch_assoc()) {
+
+			//Present the data
+			echo '<ul>';
+			echo '<li>ID: '.$row['id'].'</li>';
+			echo '<li>Name: '.$row['name'].'</li>';
+			echo '<li>Description: '.$row['description'].'</li>';
+			echo '<li>Price: '.$row['price'].'</li>';
+			echo '<li>Stock: '.$row['stock'].'</li>';
+			echo '</ul>';
+		}
+	?>
+
+	</body>
+</html>
---
+++ shopping_cart.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.7.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jan 27, 2016 at 01:51 am
+-- Server version: 5.6.20
+-- PHP Version: 5.5.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `shopping_cart`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE IF NOT EXISTS `products` (
+`id` smallint(5) unsigned NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `description` varchar(2000) NOT NULL,
+  `price` decimal(4,2) NOT NULL,
+  `stock` tinyint(4) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `description`, `price`, `stock`) VALUES
+(1, 'Apple', 'Red delicious', '1.00', 50),
+(2, 'Pear', 'Green', '1.00', 40),
+(3, 'Orange', 'Orange', '2.00', 34),
+(4, 'Plum', 'Black Doris', '1.20', 60),
+(5, 'Banana', 'Yellow', '0.80', 80),
+(6, 'Peach', 'Golden', '1.30', 27),
+(7, 'Nectarine', 'Stone fruit', '1.30', 46);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+MODIFY `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
