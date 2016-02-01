5e166a5d5555dbdcc2cdfbb0fa5bd6dd14b0f690
Benjamin Cole <bentleycolchester@gmail.com>
2016-02-01 13:25:30 +1300

    Update product cart functionality

--- index.php
+++ index.php
@@ -40,14 +40,38 @@
 
 		//Extract the data
 		$result = $result->fetch_assoc();
-		
-		//Add the item to the cart
+
+		$productFound = false;
+
+		//Loop over the cart and see if the product is added already
+		for ($i=0; $i < count($_SESSION['cart']); $i++) { 
+			
+			//Get the ID of the product in the cart
+			$cartItemID = $_SESSION['cart'][$i]['id'];
+
+			//Get the ID of the product being added to the cart
+			$addItemID = $_POST['product-id'];
+
+			//If the two ID's match
+			if ($cartItemID == $addItemID) {
+				$_SESSION['cart'][$i]['quantity'] += $_POST['quantity'];
+				$productFound = true;
+			}
+		}
+
+		//If the product was not found in the cart
+		if ( ! $productFound) {
+			
+			//Add the item to the cart
 		$_SESSION['cart'][] = [
-			'id'=>$_POST['product-id'],
-			'name'=>$_POST['name'],
-			'description'=>$_POST['description'],
-			'price'=> $result['price']
+			'id'=> $_POST['product-id'],
+			'name'=> $_POST['name'],
+			'description'=> $_POST['description'],
+			'price'=> $result['price'],
+			'quantity'=> $_POST['quantity']
 			];
+		}
+		
 	}
 
 	//Include header
