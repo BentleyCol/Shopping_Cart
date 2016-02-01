1f78665bab61220de4dcc6d2f0b8ee0249208131
Benjamin Cole <bentleycolchester@gmail.com>
2016-02-01 13:27:51 +1300

    Set up the initial checkout

---
+++ checkout.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start();
+
+//Include header template
+include 'templates/header.template.php';
+
+//Display cart contents
+include 'templates/cart-contents.template.php';
+
+
+
+
+
+
+
+
+
+
+
+//Include the footer
+include 'templates/footer.template.php';
